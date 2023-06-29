08993cb998b5c702194bfaa116fe88e97a6f0970
Adnir Andrade <adnir_andrade@hotmail.com>
2023-06-28 21:06:23 -0300

    Organizing folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -108,6 +108,26 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <div className="position-absolute top-0 start-0 bottom-0 end-0 d-flex justify-content-center align-items-center">
+        <Image
+          className="logo"
+          src="/assets/images/logo.jpg"
+          alt="logo"
+          width={800}
+          height={500}
+        />
+      </div>
+      <div className="d-flex justify-content-center align-items-center container">
+        <button
+          type="button"
+          className="btn btn-dark btn-lg mb-4 themys-button"
+        >
+          Sign In
+        </button>
+        <button type="button" className="btn btn-dark btn-lg themys-button">
+          Log In
+        </button>
+      </div>
     </main>
-  )
+  );
 }
---
+++ src/app/test.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <div className="position-absolute top-0 start-0 bottom-0 end-0 d-flex justify-content-center align-items-center">
+        <Image
+          className="logo"
+          src="/assets/resources/images/logo.jpg"
+          alt="logo"
+          width={800}
+          height={500}
+        />
+      </div>
+      <div className="d-flex justify-content-center align-items-center container">
+        <button
+          type="button"
+          className="btn btn-dark btn-lg mb-4 themys-button"
+        >
+          Sign In
+        </button>
+        <button type="button" className="btn btn-dark btn-lg themys-button">
+          Log In
+        </button>
+      </div>
+    </main>
+  );
+}
