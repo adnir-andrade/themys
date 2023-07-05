86093b0b8258d5939cbc6c6e96a84a3d325f8384
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-04 22:08:59 -0300

    Fixed the bug that was preventing Tailwind from working properly

---
+++ src/app/2page.tsx
@@ -0,0 +1,100 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-blue-500 text-white p-4 rounded">
+          <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+          <p className="mt-2">This is a TypeScript/React example.</p>
+          <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded mt-4">
+            Click me
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <p>
+          Get started by editing&nbsp;
+          <code>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <Image
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
