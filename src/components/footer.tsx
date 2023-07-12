67fac34618805c5eb90ce75ace61e13d6d860c82
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-12 18:50:17 -0300

    Footer is now sticky

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-almost-black sepia z-10 py-2 mix-blend-multiply flex justify-center text-amber-700 hover:text-amber-200 transform transition-all duration-1000 bottom-0">
+    <div className="bg-almost-black sepia z-10 py-2 mix-blend-multiply flex justify-center text-amber-700 hover:text-amber-200 transform transition-all duration-1000 bottom-0 sticky">
       <h5 className="text-sm">@ 2023 by Adnir &#39;Haotran&#39; A.</h5>
     </div>
   );
