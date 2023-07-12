5bda8b5840e414b1fd5eace7327df5a041ccabbf
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-12 18:07:19 -0300

    Update footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="bg-almost-black sepia z-10 py-2 mix-blend-multiply flex justify-center text-amber-700 hover:text-amber-200 transform transition-all duration-1000">
-      <h5 className="text-sm">
-        Designed and Developed by Adnir &#39;Haotran&#39; A.
-      </h5>
+      <h5 className="text-sm">@ 2023 by Adnir &#39;Haotran&#39; A.</h5>
     </div>
   );
 };
