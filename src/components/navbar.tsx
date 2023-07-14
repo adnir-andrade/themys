3435b5b0b34c05f150381b1709b2194241233a55
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-14 17:58:45 -0300

    Make tittle bigger

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-almost-black sepia relative z-10 mix-blend-multiply">
       <div className="container mx-auto px-12 py-3 flex items-center justify-between">
-        <div className="text-amber-200 text-xl font-bold z-10">
+        <div className="text-amber-200 text-2xl font-bold z-10">
           <Link href="/">Themys</Link>
         </div>
         <div className="space-x-4 text-amber-200">
