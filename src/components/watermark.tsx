8c5493950c2ad8b3848a4588326aaf56a606080b
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-14 16:52:23 -0300

    Adjust logo to be simply perfect

--- src/components/watermark.tsx
+++ src/components/watermark.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
-import IMAGES from '../../public/assets/images/index';
 import logo from '../../public/assets/images/logo.jpg';
 
 export default function Watermark() {
   return (
     <Image
-      className="logo mix-blend-color-burn absolute md:max-w-3xl"
+      className="logo mix-blend-color-burn absolute h-screen object-contain"
       src={logo}
       alt="logo"
     />
   );
 }
 
-// "logo mix-blend-color-burn absolute object-cover"
+// "logo mix-blend-color-burn absolute object-cover"  md:max-w-3xl
