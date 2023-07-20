c91651b873a8a260763d8e080e768e3f498c8d6f
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-20 10:09:52 -0300

    Refactor: Remove unnecessary classes

--- src/pages/select-campaign.tsx
+++ src/pages/select-campaign.tsx
@@ -15,12 +15,12 @@ export default function SelectCampaign() {
       <main className="min-[100px]:mx-[5%] sm:mx-[10%]">
         <div className="grid grid-rows-1 grid-flow-col gap-5 items-center justify-center my-10">
           <Image className="flex-none" src={lineLeft} alt="line-left" />
-          <h1 className="text-dark-sepia xsm:text-5v sm:text-[28px] md:text-[36px] text-center drop-shadow-title title">
+          <h1 className="text-dark-sepia xsm:text-5v sm:text-[28px] md:text-[36px] text-center drop-shadow-title">
             Select a Campaign
           </h1>
           <Image className="flex-none" src={lineRight} alt="line-right" />
         </div>
-        <div className="transp-bg h-4/5 xsm:w-full sm:w-auto bg-semi-transparent-black rounded-t-[20px] rounded-b-xl mb-10">
+        <div className="h-4/5 xsm:w-full sm:w-auto bg-semi-transparent-black rounded-t-[20px] rounded-b-xl mb-10">
           <div className="grid grid-cols-1 md:grid-cols-3 min-h-[60vh] justify-center text-center gap-5 mb-3 py-10 px-5">
             <p>Content goes here...</p>
             <p>Content goes here...</p>
@@ -29,17 +29,17 @@ export default function SelectCampaign() {
           <div className="flex flex-row justify-evenly pb-5 ">
             {/* TODO: Refactor semi-transp and custom-button to images bellow. Turn then into buttons */}
             <Image
-              className="p-2 hover:drop-shadow-glow duration-700"
+              className="hover:drop-shadow-glow duration-700"
               src={sortIcon}
               alt="sort"
             />
             <Image
-              className="p-2 hover:drop-shadow-glow duration-700"
+              className="hover:drop-shadow-glow duration-700"
               src={addIcon}
               alt="add"
             />
             <Image
-              className="p-2 hover:drop-shadow-glow duration-700"
+              className="hover:drop-shadow-glow duration-700"
               src={settingsIcon}
               alt="settings"
             />
