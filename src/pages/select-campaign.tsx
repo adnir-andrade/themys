a9f95d5e05ec634bf7b794e00ab098bef79b2215
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-20 07:26:29 -0300

    Refactor: Add images as required by NextJS

--- src/pages/select-campaign.tsx
+++ src/pages/select-campaign.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import lineLeft from '../../public/assets/icons/line-left.svg';
+import lineRight from '../../public/assets/icons/line-right.svg';
+import sortIcon from '../../public/assets/icons/sort.svg';
+import addIcon from '../../public/assets/icons/add.svg';
+import settingsIcon from '../../public/assets/icons/settings.svg';
 
 export default function SelectCampaign() {
   return (
@@ -10,13 +15,13 @@ export default function SelectCampaign() {
       <main>
         <div className="d-flex flex-row mb-3 align-items-center justify-content-center p-3">
           <div className="p-2">
-            <img src="/assets/resources/icons/line-left.svg" />
+            <Image className="" src={lineLeft} alt="line-left" />
           </div>
           <div className="p-2">
             <h1 className="themys-font-bold title">Select a Campaign</h1>
           </div>
           <div className="p-2">
-            <img src="/assets/resources/icons/line-right.svg" />
+            <Image className="" src={lineRight} alt="line-right" />
           </div>
         </div>
         <div className="position-relative d-flex flex-column mb-3 align-items-center">
@@ -28,13 +33,13 @@ export default function SelectCampaign() {
                 className="d-flex flex-row mb-3 align-items-center justify-content-evenly"
               >
                 <button className="p-2 semi-transp custom-button" id="sort">
-                  <img src="/assets/resources/icons/sort.svg" />
+                  <Image className="" src={sortIcon} alt="sort" />
                 </button>
                 <button className="p-2 semi-transp custom-button" id="add">
-                  <img src="/assets/resources/icons/add.svg" />
+                  <Image className="" src={addIcon} alt="add" />
                 </button>
                 <button className="p-2 semi-transp custom-button" id="settings">
-                  <img src="/assets/resources/icons/settings.svg" />
+                  <Image className="" src={settingsIcon} alt="settings" />
                 </button>
               </div>
             </div>
