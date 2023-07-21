78f18a60c7b39ce063f1aed1c4e34b5ddf7ff8a3
Adnir Andrade <adnir_andrade@hotmail.com>
2023-07-21 11:43:46 -0300

    Encapsulate: Extract page title into a reusable component

---
+++ src/components/page-title.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import lineLeft from '../../public/assets/icons/line-left.svg';
+import lineRight from '../../public/assets/icons/line-right.svg';
+
+type Props = {
+  pageTitle: string;
+};
+
+const PageTitle = ({ pageTitle }: Props) => {
+  return (
+    <div className="grid grid-rows-1 grid-flow-col gap-5 items-center justify-center my-10">
+      <Image className="flex-none" src={lineLeft} alt="line-left" />
+      <h1 className="text-dark-sepia xsm:text-5v sm:text-[28px] md:text-[36px] text-center drop-shadow-title">
+        {pageTitle}
+      </h1>
+      <Image className="flex-none" src={lineRight} alt="line-right" />
+    </div>
+  );
+};
+
+export default PageTitle;
--- src/pages/select-campaign.tsx
+++ src/pages/select-campaign.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import lineLeft from '../../public/assets/icons/line-left.svg';
-import lineRight from '../../public/assets/icons/line-right.svg';
 import sortIcon from '../../public/assets/icons/sort.svg';
 import addIcon from '../../public/assets/icons/add.svg';
 import settingsIcon from '../../public/assets/icons/settings.svg';
 import TranslucentContainer from '@/components/translucent-box';
+import PageTitle from '@/components/page-title';
 
 export default function SelectCampaign() {
   return (
@@ -14,13 +13,7 @@ export default function SelectCampaign() {
         <title>Select Campaign</title>
       </Head>
       <main className="min-[100px]:mx-[5%] sm:mx-[10%]">
-        <div className="grid grid-rows-1 grid-flow-col gap-5 items-center justify-center my-10">
-          <Image className="flex-none" src={lineLeft} alt="line-left" />
-          <h1 className="text-dark-sepia xsm:text-5v sm:text-[28px] md:text-[36px] text-center drop-shadow-title">
-            Select a Campaign
-          </h1>
-          <Image className="flex-none" src={lineRight} alt="line-right" />
-        </div>
+        <PageTitle pageTitle="Select a Campaign" />
         <TranslucentContainer>
           <div className="grid grid-cols-1 md:grid-cols-3 min-h-[60vh] justify-center text-center gap-5 mb-3 py-10 px-5">
             <p>Content goes here...</p>
