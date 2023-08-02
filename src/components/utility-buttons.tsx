c0022bfe43f63974bec179a833f75276dc32b5a1
Adnir Andrade <adnir_andrade@hotmail.com>
2023-08-02 09:36:58 -0300

    Make utility buttons into proper buttons

--- src/components/utility-buttons.tsx
+++ src/components/utility-buttons.tsx
@@ -3,25 +3,39 @@ import sortIcon from '../../public/assets/icons/sort.svg';
 import addIcon from '../../public/assets/icons/add.svg';
 import settingsIcon from '../../public/assets/icons/settings.svg';
 
+const clickHandler = () => {
+  console.log('Testing click handler');
+};
+
+const addCampaign = () => {
+  console.log('Testing add campaign');
+};
+
 export default function UtilityButtons() {
   return (
     <div className="flex flex-row justify-evenly pb-5 ">
       {/* TODO: Refactor - Convert those to buttons */}
-      <Image
-        className="hover:drop-shadow-glow duration-700"
-        src={sortIcon}
-        alt="sort"
-      />
-      <Image
-        className="hover:drop-shadow-glow duration-700"
-        src={addIcon}
-        alt="add"
-      />
-      <Image
-        className="hover:drop-shadow-glow duration-700"
-        src={settingsIcon}
-        alt="settings"
-      />
+      <button onClick={clickHandler}>
+        <Image
+          className="hover:drop-shadow-glow duration-700"
+          src={sortIcon}
+          alt="sort"
+        />
+      </button>
+      <button onClick={addCampaign}>
+        <Image
+          className="hover:drop-shadow-glow duration-700"
+          src={addIcon}
+          alt="add"
+        />
+      </button>
+      <button onClick={clickHandler}>
+        <Image
+          className="hover:drop-shadow-glow duration-700"
+          src={settingsIcon}
+          alt="settings"
+        />
+      </button>
     </div>
   );
 }
